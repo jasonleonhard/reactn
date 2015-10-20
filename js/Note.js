e7eff1b6e8456d214fb4cf387d16d3cc1b362d82
un5t0ppab13 <devbrights@gmail.com>
2015-10-20 14:21:47 -0700

    refactor notes

--- js/Note.js
+++ js/Note.js
@@ -1,26 +1,20 @@
 // Renders a yellow sticky note with words penciled on
 // great example of state, triggered by events, such as clicking specific buttons
-// component 1 Note
+
+// Component 1 Note:
 var Note = React.createClass({
     getInitialState: function() {
-        // alert('getInitialState note');
         return {editing: false}         // cannot edit on initialize
     },
     edit: function(){
-        // alert('editing note');       // <button onClick={this.edit}
         this.setState({editing: true})  // can edit while true
     },
     save: function() {
-        // alert('saving note');
         this.props.onChange(this.refs.newText.getDOMNode().value, this.props.index)
-        // var val = this.refs.newText.getDOMNode().value; // newText variable will grab value of that text area
-        // alert('saving note not fully implimented\n' + val);
         this.setState({editing: false}); // when saved no more editing
     },
     remove: function(){
         this.props.onRemove(this.props.index);
-        // alert('removing note');
-        // this.setState({removing: true});
     },
     renderDisplay: function() {
         return (
@@ -51,23 +45,10 @@ var Note = React.createClass({
             return this.renderDisplay();
         }
     }
-    // render: function(){
-    //     return (
-    //         <div className="note">
-    //             <p>{this.props.children}</p>    // allows INNERhtml to be visible
-    //             <span>
-    //                 <button onClick={this.edit} // attaches function to button
-    //                     className="btn btn-primary glyphicon glyphicon-pencil"/>
-    //                 <button onClick={this.remove}
-    //                     className="btn btn-danger glyphicon glyphicon-trash"/>
-    //             </span>
-    //         </div>
-    //     )
-    // }
 });
 
 
-// component 2 Board, propTypes are validations
+// Component 2 Board: propTypes are validations
 var Board = React.createClass({
     propTypes: {
         count: function(props, propName){
@@ -131,26 +112,9 @@ var Board = React.createClass({
                         onClick={this.add.bind(null, "new note")}> </button>
             </div>
         );
-        // return( 
-        //     <div className="board">
-        //         {this.state.notes.map(function(note,i){ 
-        //             return (<Note key={i}>{note}</Note>); // moved to eachNote
-        //         })}
-        //     </div>
-        // );
-        // return <div className="board">{this.props.count}</div>
     }
 });
 
-// render component 1 & 2 simultaneously 
+// Render Components 1 & 2 simultaneously 
 React.render(<Board count={10}/>, 
     document.getElementById('react-container'));
-
-// // render component 2
-// React.render(<Board count={10}/>, 
-//     document.getElementById('react-container'));
-
-// // render component 1
-// // this.props.children above allows this text
-// React.render(<Note>Hello World</Note>, 
-//     document.getElementById('react-container'));
